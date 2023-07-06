8dc81d510707d40242c2202c4edcd56f6b14920c
Ricardo <dasilvaricardog@gmail.com>
2023-07-06 13:10:43 +0200

    Comienzo de proyecto final

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Navbar from "./layout/navbar/Navbar";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/layout/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    
+      
+  );
+};
+
+export default Navbar;
